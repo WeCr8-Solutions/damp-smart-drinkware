2f8391d12c2d392545e517439750805e0e6379ba
WeCr8 <zach@wecr8.info>
2025-10-13 16:25:08 -0700

    Update Stripe function to force redeploy with new key

--- netlify/functions/create-checkout-session.js
+++ netlify/functions/create-checkout-session.js
@@ -1,7 +1,7 @@
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 exports.handler = async (event, context) => {
-    // Enable CORS
+    // Enable CORS - Updated with new Stripe key
     const headers = {
         'Access-Control-Allow-Origin': '*',
         'Access-Control-Allow-Headers': 'Content-Type',
